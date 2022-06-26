c54eb261fcd47393e5c2a371027c3c4781044da2
IsmailYOmar <ismailyomar@gmail.com>
2022-06-26 21:23:19 +0200

    CollectionDetailsActivity works only % left

--- app/src/main/java/com/opsc/collectebils/CollectionDetailsActivity.java
+++ app/src/main/java/com/opsc/collectebils/CollectionDetailsActivity.java
@@ -4,12 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -17,8 +20,12 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -40,7 +47,7 @@ public class CollectionDetailsActivity extends AppCompatActivity {
     private DatabaseReference ref;
     private DatabaseReference ref2;
 
-
+    ArrayList<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,30 +63,18 @@ public class CollectionDetailsActivity extends AppCompatActivity {
         catName = getIntent().getExtras().getString("collectionName");
         catKey = getIntent().getExtras().getString("collectionKey");
 
-        categoryName.setText(catName);
-        GetCollectionDetails();
-        categoryGoal.setText(goalNumber);
-        GetItemDetails();
-        itemNumber.setText(count);
-
-        if(goalNumber != 0) {
-            double percent = (count / goalNumber) * 100;
-            itemPercentage.setText(percent + "%");
-        }else{
-            itemPercentage.setText("0%");
-        }
-
-
-        //progressBar.setMax(goalNumber);
-        //progressBar.setProgress(count);
-
-    }
-
-    public void GetCollectionDetails() {
-        // get categoryGoal
         user = FirebaseAuth.getInstance().getCurrentUser();
         assert user != null;
         userId = user.getUid();
+
+        categoryName.setText(catName);
+        GetCollectionDetails(userId);
+        GetItemDetails(userId);
+
+    }
+
+    public void GetCollectionDetails(String userId) {
+        // get categoryGoal
         ref=FirebaseDatabase.getInstance().getReference("Categories");
 
         ref.orderByChild("userID").equalTo(userId).addChildEventListener(new ChildEventListener()
@@ -88,6 +83,8 @@ public class CollectionDetailsActivity extends AppCompatActivity {
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 Category value = snapshot.getValue(Category.class);
                 if (value.getCategoryName().equals(catName) && snapshot.getKey().equals(catKey)) {
+                    categoryGoal.setText(String.valueOf(value.getGoalNumber()));
+                    progressBar.setMax(value.getGoalNumber());
                     goalNumber = value.getGoalNumber();
                 }
             }
@@ -118,46 +115,58 @@ public class CollectionDetailsActivity extends AppCompatActivity {
         });
 
     }
-    public void GetItemDetails() {
 
-        //get current user and create firebase instance
+    public void GetItemDetails(String userId) {
+
         ref2 = FirebaseDatabase.getInstance().getReference("Items");
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        assert user != null;
-        userId = user.getUid();
-        ref2.orderByChild("userID").equalTo(userId).addChildEventListener(new ChildEventListener()
-        {
+
+
+        ref2.orderByChild("userID").equalTo(userId).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName)
             {
-                //save firebase query result to object of Items class
                 Items value2 = snapshot.getValue(Items.class);
-                if (value2.getCategoryKey().equals(catKey) && value2.categoryName.equals(catName) ){
+                if (value2.getCategoryKey().equals(catKey) && value2.categoryName.equals(catName) && value2.getUserID().equals(userId))
+                {
+                    list.add(value2.getItemName());
+                }
 
-                    count= count+1;
+                itemNumber.setText(String.valueOf(list.size()));
+                progressBar.setProgress(list.size());
+
+                if(goalNumber != 0) {
+                    double percent = (list.size() / goalNumber) * 100;
+                    itemPercentage.setText(String.valueOf(percent) + "%");
+                }else{
+                    itemPercentage.setText("0%");
                 }
             }
 
             @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName)
+            {
 
             }
 
             @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            public void onChildRemoved(@NonNull DataSnapshot snapshot)
+            {
 
             }
 
             @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName)
+            {
 
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onCancelled(@NonNull DatabaseError error)
+            {
 
             }
         });
+
     }
 
 }
